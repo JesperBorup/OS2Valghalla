591ce9a161880080d7225795eb0e03560da4e37a
jesper-mb <47184741+jesper-mb@users.noreply.github.com>
2023-04-11 10:04:26 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OS2Valghalla'
+copyright = '2023, Precio Fishbone'
+author = 'Precio Fishbone'
 
 release = '0.1'
 version = '0.1.0'
